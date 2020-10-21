b629382f17d43de31a2248920350fc7936027e64
Sampada-VS <samssvv1@gmail.com>
2020-10-22 03:29:42 +0530

    Adding play options-no play,snake,ladder

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,7 +1,22 @@
 public class SnakeLadder {
-
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
+	
 	public static void main(String[] args) {
 		int playerPosition = 0;
 		int diceNum = ((int)((Math.random()*10)%6))+1;
+		int playOption = (int)((Math.random()*10)%3);
+
+		switch (playOption) {
+			case NO_PLAY :	
+				break;
+			case LADDER :
+				playerPosition = playerPosition + diceNum;
+				break;
+			case SNAKE :
+				playerPosition = playerPosition - diceNum;
+				break;
+		}
 	}
 }
