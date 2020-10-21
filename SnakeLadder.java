d9e9cc0963aac1ecf8072a51a08b0bccb346ab72
Sampada-VS <samssvv1@gmail.com>
2020-10-22 04:05:32 +0530

    Adding feature- display winner & if climbed ladder play again

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,34 +1,45 @@
-public class SnakeLadder {
+class Player {
 	public static final int NO_PLAY=0;
 	public static final int LADDER=1;
 	public static final int SNAKE=2;
-	
-	public static void main(String[] args) {
-		int playerPosition = 0;
-		int numOfRolls = 0;
-		while (playerPosition < 100) {
+
+	public int play(int playerPosition) {
+		
+		int diceNum = ((int)((Math.random()*10)%6))+1;
+		int playOption = (int)((Math.random()*10)%3);
 			
-			int diceNum = ((int)((Math.random()*10)%6))+1;
-			numOfRolls++;
-			int playOption = (int)((Math.random()*10)%3);
-			
-			switch (playOption) {
-				case NO_PLAY :	
-					break;
-				case LADDER :
-					playerPosition = playerPosition + diceNum;
-					break;
-				case SNAKE :
-					playerPosition = playerPosition - diceNum;
-					break;
-			}
-			if (playerPosition < 0)
-				playerPosition = 0;
-			if (playerPosition > 100)
+		switch (playOption) {
+			case NO_PLAY :	
+				break;
+			case LADDER :
+				playerPosition = playerPosition + diceNum;
+				play(playerPosition);
+				break;
+			case SNAKE :
 				playerPosition = playerPosition - diceNum;
-			System.out.println("After "+numOfRolls+" roll , Player position: "+playerPosition);
+				break;
 		}
-		System.out.println("Number of times dice was rolled :"+numOfRolls);
-		System.out.println("Player wins with position :"+playerPosition);
+		if (playerPosition < 0)
+			playerPosition = 0;
+		if (playerPosition > 100)
+			playerPosition = playerPosition - diceNum;
+		return playerPosition;
+	}
+}
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int player1Position = 0;
+		int player2Position = 0;
+		Player player1=new Player();
+		Player player2=new Player();
+
+		while (player1Position < 100 && player2Position < 100) {
+			player1Position = player1.play(player1Position);
+			player2Position = player2.play(player2Position);
+		}
+		if (player1Position == 100)
+			System.out.println("Player1 wins the game.");
+		else
+			System.out.println("Player2 wins the game.");
 	}
 }
